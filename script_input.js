ae20b0c292aeaa354ee1b3ba7ba7b7fa9ba00eb1
Vinicius Braznick <viniciusbraznick@gmail.com>
2020-06-14 17:19:17 -0300

    Desktop version complete

--- script_input.js
+++ script_input.js
@@ -1,11 +1,13 @@
 function validaEmail() {
     email = document.getElementById('email').value
-
-    if(email.length > 0){
-       if(email.search("@") != -1 && email.search(".com") != -1){
+    
+       if(email.search("@") != -1 && email.search(".com") != -1 && email.length > 5){
             console.log('emial valido')
+            document.getElementById('email').value = ""
+            document.getElementById("erro").style.display = "none"
+            document.getElementById("textErro").style.display = "none"
        } else{
-            console.log('email invalido')
+            document.getElementById("erro").style.display = "block"
+            document.getElementById("textErro").style.display = "block"
        }
-    }
 }
